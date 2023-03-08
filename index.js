e2734238bed09c66c25a94f5e1d9c754e25100ab
Engxli <baaa555@outlook.com>
2023-03-08 19:23:50 +0300

    TASK OBJECTS

---
+++ index.js
@@ -0,0 +1,21 @@
+const books = require("./index.json");
+
+/*
+================================================================================
+Your task is to write a function that takes in this array of book
+objects and returns an array of all the titles of books that are available.
+
+The returned array should have the following structure:
+[
+  "The Great Gatsby",
+  "Pride and Prejudice",
+  "To Kill a Mockingbird",
+  // ...
+]
+================================================================================
+*/
+function getAvailableBooks(books) {
+  // Your code here
+}
+
+console.log(getAvailableBooks(books));
